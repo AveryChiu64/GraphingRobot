1e363f40d256b51da9cfd9c25fca451c61139f35
Avery Chiu <averychiuu@gmail.com>
2019-10-17 09:45:59 -0400

    Initial commit for graphing robot

---
+++ InterpretFunction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	
+	//Main Menu
+	cout <<"Welcome to the robot graphing calculator" << endl;
+	cout <<"This program accepts a function and graphs the robot" << endl;
+	cout <<"Please pick an option" << endl;
+	
+	
+	int optionOfFunction=0;
+	//Create a switch case for this 
+	cout << setw(5) << "1." << "1" << endl;
+	cout << setw(5) << "2." << "2" << endl;
+	cout << setw(5) << "3." << "3" << endl;
+	cout << setw(5) << "4." << "4" << endl;
+	cout << setw(5) << "5." << "5" << endl;
+	//take in values like a button on calculator 				
+	//break down each part of the function into its smaller compoonents
+	//eg ln x should be a function
+	//cos 2x should be a seperate function
+	//allow the user to concatenate the string to create amassive funtion
+	
+}
+
+
