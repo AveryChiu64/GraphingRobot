a2d5a19ec3ee2394dbe9cab0681da7ae47e10d5b
Avery Chiu <averychiuu@gmail.com>
2019-10-17 10:26:18 -0400

    Started function for polynomials and created main menu

--- InterpretFunction.cpp
+++ InterpretFunction.cpp
@@ -1,3 +1,10 @@
+/*
+This progarm is created to accept basic functions and interpret them for a 
+LEGO EV3 Robot to plot out on a graph. This robot will draw the curve
+on either a whiteboard or a piece of paper.
+*/
+
+//Libraries
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -5,27 +12,42 @@
 
 using namespace std;
 
-int main() {
-	
+int main() {//Main
+	int userInput=0;
 	//Main Menu
 	cout <<"Welcome to the robot graphing calculator" << endl;
-	cout <<"This program accepts a function and graphs the robot" << endl;
+	cout <<"This program accepts basic functions and graphs the robot" << endl;
+	cout <<"These functions will generally be in the form of y=af(kx)+c" << endl;
+
+	//Continually checks for proper input 
+	do {
 	cout <<"Please pick an option" << endl;
+	cout <<"1. Polynomial function" << endl;
+	cout <<"2. Rational function" << endl;
+	cout <<"3. Trigonometric function" << endl;
+	cout <<"4. Logarithmic function" << endl;
+	cout <<"5. Exponential function" << endl;
+	}while(userInput<1 && userInput > 5);
 	
-	
-	int optionOfFunction=0;
-	//Create a switch case for this 
-	cout << setw(5) << "1." << "1" << endl;
-	cout << setw(5) << "2." << "2" << endl;
-	cout << setw(5) << "3." << "3" << endl;
-	cout << setw(5) << "4." << "4" << endl;
-	cout << setw(5) << "5." << "5" << endl;
-	//take in values like a button on calculator 				
-	//break down each part of the function into its smaller compoonents
-	//eg ln x should be a function
-	//cos 2x should be a seperate function
-	//allow the user to concatenate the string to create amassive funtion
-	
+}
+
+//Proccesses a polynomial
+bool polynomial() {
+	int degree=0;
+cout <<" What degree would you like your polynomial to be?" << endl;
+cout <<" Please enter a number between 1 and 10 inclusive?" << endl;
+cin >> degree;
+cout <<" You have chosen a " << degree << "funtion" endl;
+//Calls the polynomial degree funtion to process the funtion
+polynomialDegree(degree);
+}
+
+string polynomialDegree(int degree) {
+	int coefficients[degree];
+	for(int countDegree=0;countDegree<degree<countDegree++){//accept each coefficients
+	cin >> coefficients[countDegree];
+	}
+
 }
 
 
