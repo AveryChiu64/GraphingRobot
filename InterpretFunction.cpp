26065fd7fcdc10f7a8ffc014723f620289a2fc2d
Avery Chiu <averychiuu@gmail.com>
2019-10-18 08:45:19 -0400

    Adjusted formatting for polynomial output and naming convention of the functions

--- InterpretFunction.cpp
+++ InterpretFunction.cpp
@@ -2,6 +2,10 @@
 This progarm is created to accept basic functions and interpret them for a 
 LEGO EV3 Robot to plot out on a graph. This robot will draw the curve
 on either a whiteboard or a piece of paper.
+
+Sources:
+http://www.cplusplus.com/doc/tutorial/exceptions/
+http://www.cplusplus.com/doc/tutorial/arrays/
 */
 
 //Libraries
@@ -9,19 +13,29 @@ on either a whiteboard or a piece of paper.
 #include <cstdlib>
 #include <cmath>
 #include <iomanip>
-//function prototypes
-void polynomial();
+
+//Function prototypes
+void getFunction();
+int degreeInput();
 void polynomialDegree(int degree);
+
 using namespace std;
 
-
-int main() {//Main
-	int userInput=0;
+//main, calls the other functions
+int main() {
 	//Main Menu
 	cout <<"Welcome to the robot graphing calculator" << endl;
 	cout <<"This program accepts basic functions and graphs the robot" << endl;
 	cout <<"These functions will generally be in the form of y=af(kx)+c" << endl;
 
+	getFunction();
+	system("PAUSE");
+	return 0;
+}
+
+void getFunction() {
+
+int userInput=0;
 	//Continually checks for proper input 
 	do {
 	cout <<"Please pick an option" << endl;
@@ -33,40 +47,46 @@ int main() {//Main
 	cin >> userInput;
 	}while(userInput < 1 || userInput > 5);
 	
+	//checks the option the user entered
 	if(userInput==1)
-	polynomial();
-
-	system("PAUSE");
-	return 0;
+	polynomialDegree(degreeInput());
 }
 
-//Proccesses a polynomial
-void polynomial() {
+//Allows the user to choose a degree of their function(s)
+int degreeInput() {
 	int degree=0;
 cout <<" What degree would you like your function to be?" << endl;
 do{
 cout <<" Please enter a number between 1 and 10 inclusive?" << endl;
 cin >> degree;
 }while(degree < 0 || degree > 10 );
-cout <<" You have chosen a " << degree << "function" << endl;
-//Calls the polynomialDegree function to process the polynomial
-polynomialDegree(degree);
+cout <<" You have chosen to make a " << degree << "th degree function" << endl;
+return degree;
 }
 
+//Procceses the degree of the polynomial and shows the user their polynomial 
 void polynomialDegree(int degree) {
 	int coefficients[degree];
 	int constant=0;
 
-	for(int countDegree=0;countDegree<degree;countDegree++){//accept each coefficients
-	cout << "Please enter the coefficient for the x value of degree " << countDegree << endl;
+	//Accepts each coefficient of the function and then the constant value c 
+	for(int countDegree=degree-1;countDegree>=0;countDegree--){
+	cout << "Please enter the coefficient for the x^" << countDegree+1 << endl;
 	cin >> coefficients[countDegree];
 	}
 	cout <<"Please enter the constant value c" << endl;
 	cin >> constant;
 
 	cout <<"Your polynomial is ";
-	for(int countDegree=degree-1;countDegree>=0;countDegree--)
-	cout << coefficients[countDegree] << "x^ + " << countDegree;
+
+	//Prints out the function for the user to see
+	for(int countDegree=degree-1;countDegree>=0;countDegree--) {
+		if(countDegree!=0 && coefficients[countDegree]!=0)
+		cout << coefficients[countDegree] << "x^" << countDegree+1 << " + ";
+		else if (countDegree==0 && coefficients[countDegree]!=0)
+		cout << coefficients[countDegree] << "x" <<" + ";
+		
+	}
 	cout << constant << endl;
 
 }
