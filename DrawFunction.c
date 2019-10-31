6d21f6f69ae1ea7a5767f0993543f2975ae9be88
Avery Chiu <a27chiu@uwaterloo.ca>
2019-10-31 10:47:32 -0400

    Created function to get coordinates from a file

---
+++ DrawFunction.c
@@ -0,0 +1,54 @@
+//This is for RobotC
+
+//Sources
+//https://overiq.com/c-programming-101/array-of-structures-in-c/
+//https://www.geeksforgeeks.org/structures-c/
+
+
+//strucs
+typedef struct{
+    int x[20];
+    int y[20];
+}xyCoord;
+
+//Function Prototypes
+void readFile(xy* coords);
+
+
+//Controls calling of other functions 
+task main() {
+
+//Variables
+ xyCoord coords[20] = {{0,0},{0,0}}
+
+readFile(coords);
+startBot();
+
+}//end of main
+
+//Reads the data from the file 
+void readFile(xyCoord* coords) {
+    TFileHandle fin;
+    
+    //Check if file can open 
+    if(!openReadPC(fin,"num.txt")) {
+        displayString(3,"Error with the file")
+    }
+    else{
+        //store all coordinates in an array 
+        for(int numCoords=0;numCoords<20;numCoords++){
+            readIntPC(fin,coords[numCoords].x);
+            readIntPC(fin,coords[numCoords].y);
+        }
+    }
+    closeFilePC(fin);
+}
+
+//Waits for user to push button and release 
+void startBot(){
+displayString(3,"Please press the down button for the robot to start graphing");
+while(!getButtonPress(buttonAny)){}
+while(getButtonPress(buttonAny)){}
+displayString(3,"");
+}
+
